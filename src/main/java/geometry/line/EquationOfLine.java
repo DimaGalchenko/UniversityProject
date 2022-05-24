a911f9ca1cffd5f88ea9a56a6bc793930eff4de2
DimaGalchenko <galchenkodima31@gmail.com>
2022-05-24 18:40:41 +0300

    Refactoring

--- src/main/java/geometry/line/EquationOfLine.java
+++ src/main/java/geometry/line/EquationOfLine.java
@@ -3,18 +3,24 @@ package geometry.line;
 import java.util.Arrays;
 
 public class EquationOfLine {
-    public static final int POINT = 2;
+    public static final int POINT_COORDINATE_COUNT = 2;
+    public static final int TWO_POINT_COUNT = 2;
+    public static final int X_INDEX = 0;
+    public static final int Y_INDEX = 1;
+    public static final int A_COEFFICIENT_OF_LINE_EQUATION = 0;
+    public static final int B_COEFFICIENT_OF_LINE_EQUATION = 1;
+    public static final int C_COEFFICIENT_OF_LINE_EQUATION = 2;
 
     public static float[] by(float[][] twoPoints) throws EquationOfLineException {
-        validatePointsCount(2, twoPoints);
+        validatePointsCount(TWO_POINT_COUNT, twoPoints);
 
         float[] firstPoint = validateAndGetPoint(twoPoints[0]);
         float[] secondPoint = validateAndGetPoint(twoPoints[1]);
 
-        float a = secondPoint[1] - firstPoint[1];
-        float b = firstPoint[0] - secondPoint[0];
-        float c = - a * firstPoint[0] - b * firstPoint[1];
-        return new float[] {a, b, c};
+        float a = secondPoint[Y_INDEX] - firstPoint[Y_INDEX];
+        float b = firstPoint[X_INDEX] - secondPoint[X_INDEX];
+        float c = -a * firstPoint[X_INDEX] - b * firstPoint[Y_INDEX];
+        return new float[]{a, b, c};
     }
 
     public static float[] parallelBy(float[][] twoPoints, float[] pointOfParallelLine) throws EquationOfLineException {
@@ -26,25 +32,25 @@ public class EquationOfLine {
     }
 
     private static float[] by(LinePosition linePosition, float[][] twoPoints, float[] pointOfAnotherLine) throws EquationOfLineException {
-        validatePointsCount(2, twoPoints);
+        validatePointsCount(TWO_POINT_COUNT, twoPoints);
         validateAndGetPoint(pointOfAnotherLine);
 
         float[] firstLineCoefficients = EquationOfLine.by(twoPoints);
 
         float secondYCoordOfParallelLine = 1;
-        float a1y2 = firstLineCoefficients[0] * secondYCoordOfParallelLine;
-        float a1y1 = firstLineCoefficients[0] * pointOfAnotherLine[1];
-        float b1x1 = firstLineCoefficients[1] * pointOfAnotherLine[0];
+        float a1y2 = firstLineCoefficients[A_COEFFICIENT_OF_LINE_EQUATION] * secondYCoordOfParallelLine;
+        float a1y1 = firstLineCoefficients[A_COEFFICIENT_OF_LINE_EQUATION] * pointOfAnotherLine[Y_INDEX];
+        float b1x1 = firstLineCoefficients[B_COEFFICIENT_OF_LINE_EQUATION] * pointOfAnotherLine[X_INDEX];
 
-        if(linePosition == LinePosition.PARALLEL) {
+        if (linePosition == LinePosition.PARALLEL) {
             a1y2 = a1y2 * (-1);
         }
-        if(linePosition == LinePosition.ORTHOGONAL) {
+        if (linePosition == LinePosition.ORTHOGONAL) {
             a1y1 = a1y1 * (-1);
         }
 
-        float secondXCoordOfParallelLine = (a1y1 + a1y2 + b1x1) / firstLineCoefficients[1];
-        return EquationOfLine.by(new float[][] {
+        float secondXCoordOfParallelLine = (a1y1 + a1y2 + b1x1) / firstLineCoefficients[B_COEFFICIENT_OF_LINE_EQUATION];
+        return EquationOfLine.by(new float[][]{
                 pointOfAnotherLine,
                 {secondXCoordOfParallelLine, secondYCoordOfParallelLine}
         });
@@ -56,7 +62,7 @@ public class EquationOfLine {
     }
 
     private static void validatePointsCount(int countOfPoints, float[][] points) throws EquationOfLineException {
-        if(points.length != countOfPoints) {
+        if (points.length != countOfPoints) {
             throw new EquationOfLineException(
                     String.format("%s points must be passed, but were passed: %s", countOfPoints, points.length)
             );
@@ -64,7 +70,7 @@ public class EquationOfLine {
     }
 
     private static void validatePoint(float[] point) throws EquationOfLineException {
-        if(point.length != POINT) {
+        if (point.length != POINT_COORDINATE_COUNT) {
             throw new EquationOfLineException(
                     String.format(
                             "The point must consist of two coordinates [x, y], but now consist of: %s",
