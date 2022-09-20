07e54c4ce01d8a72125f9a773b90d34023aca71e
DimaGalchenko <galchenkodima31@gmail.com>
2022-09-20 21:30:22 +0300

    Refactor CirclesIntersection. Added method to find intersection points of line and circle

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,9 +1,6 @@
 import geometry.circle.CirclesIntersection;
 import geometry.line.EquationOfLine;
 import processing.core.PApplet;
-import processing.core.PVector;
-
-import java.util.Arrays;
 
 public class Application extends PApplet {
 
@@ -13,7 +10,7 @@ public class Application extends PApplet {
     }
 
     public void draw() {
-        background(1200f);
+//        background(1200f);
 
 //        for(int i = 0; i < 600; i++) {
 //
@@ -45,34 +42,53 @@ public class Application extends PApplet {
 //        }
 
 
-        for(int i = 50; i < 600; i=i+10) {
-            float y1 = (float) (Math.sin(i / 100.0) * 40);
-            float y2 = (float) (Math.cos(i / 100.0) * 40);
-
-            float r1 = 30;
-            float[][] circleCoord1 = new float[][]{{i, y1 + 300}, {r1}};
-
-            float r2 = 30;
-            float[][] circleCoord2 = new float[][]{{i, y2 + 300}, {r2}};
-
-
-            float[][] iter = CirclesIntersection.findIntersectionOfTwoCircles(
-                    circleCoord1,
-                    circleCoord2
+//        for(int i = 50; i < 600; i=i+10) {
+//            float y1 = (float) (Math.sin(i / 100.0) * 40);
+//            float y2 = (float) (Math.cos(i / 100.0) * 40);
+//
+//            float r1 = 30;
+//            float[][] circleCoord1 = new float[][]{{i, y1 + 300}, {r1}};
+//
+//            float r2 = 30;
+//            float[][] circleCoord2 = new float[][]{{i, y2 + 300}, {r2}};
+//
+//
+//            float[][] iter = CirclesIntersection.findIntersectionOfTwoCircles(
+//                    circleCoord1,
+//                    circleCoord2
+//            );
+//
+//            float[] i1 = iter[0];
+//            float[] i2 = iter[1];
+//
+//            color(255);
+//            noFill();
+//
+//            circle(circleCoord1[0][0], circleCoord1[0][1], r1 * 2);
+//            circle(circleCoord2[0][0], circleCoord2[0][1], r2 * 2);
+//            circle(i1[0], i1[1], 5);
+//            circle(i2[0], i2[1], 5);
+//            return;
+//        }
+//                    color(255);
+//            noFill();
+        noFill();
+        float[][] circle = new float[][]{{300, 300}, {30}};
+        float[] line = EquationOfLine.equationOfLineBy(new float[][]{{0,274},{600,274}});
+        float[][] iter = CirclesIntersection.findIntersectionOfCircleAndLine(
+                circle,
+                line
             );
 
-            float[] i1 = iter[0];
-            float[] i2 = iter[1];
+        circle(circle[0][0], circle[0][1], circle[1][0] * 2);
+        line(0,274, 600, 274);
 
-            color(255);
-            noFill();
+        float[] i1 = iter[0];
+        float[] i2 = iter[1];
 
-            circle(circleCoord1[0][0], circleCoord1[0][1], r1 * 2);
-            circle(circleCoord2[0][0], circleCoord2[0][1], r2 * 2);
-            circle(i1[0], i1[1], 5);
-            circle(i2[0], i2[1], 5);
-            return;
-        }
+        fill(255);
+        circle(i1[0], i1[1], 5);
+        circle(i2[0], i2[1], 5);
     }
 
     public static void main(String... args) {
--- src/main/java/geometry/circle/CirclesIntersection.java
+++ src/main/java/geometry/circle/CirclesIntersection.java
@@ -2,6 +2,8 @@ package geometry.circle;
 
 import processing.core.PVector;
 
+import java.util.Arrays;
+
 import static geometry.constants.CoordinateIndex.X_INDEX;
 import static geometry.constants.CoordinateIndex.Y_INDEX;
 import static processing.core.PVector.dist;
@@ -13,24 +15,34 @@ public abstract class CirclesIntersection {
             float[][] firstCircleParameters,
             float[][] secondCircleParameters
     ) {
-        validateCircleParameters(firstCircleParameters, secondCircleParameters);
+        validateCircleParameters(firstCircleParameters);
+        validateCircleParameters(secondCircleParameters);
 
         validateIfCircleIntersects(firstCircleParameters, secondCircleParameters);
 
         return findCirclesIntersects(firstCircleParameters, secondCircleParameters);
     }
 
-    private static void validateCircleParameters(float[][] firstCircleParameters, float[][] secondCircleParameters) {
-        if (firstCircleParameters.length != 2 && secondCircleParameters.length != 2) {
+    public static float[][] findIntersectionOfCircleAndLine(float[][] circleParameters, float[] lineParameters) {
+        validateCircleParameters(circleParameters);
+        validateLineParameters(lineParameters);
+
+        return findCircleLineIntersects(circleParameters, lineParameters);
+    }
+
+    private static void validateCircleParameters(float[][] circleParameters) {
+        if (circleParameters.length != 2) {
             throw new IllegalArgumentException("Invalid parameters");
         }
 
-        if (firstCircleParameters[0].length != 2 && firstCircleParameters[1].length != 1) {
-            throw new IllegalArgumentException("Invalid first parameter, should [[xCenter, yCenter], [radius]]");
+        if (circleParameters[0].length != 2 && circleParameters[1].length != 1) {
+            throw new IllegalArgumentException("Invalid parameters, should [[xCenter, yCenter], [radius]]");
         }
+    }
 
-        if (secondCircleParameters[0].length != 2 && secondCircleParameters[1].length != 1) {
-            throw new IllegalArgumentException("Invalid second parameter, should [[xCenter, yCenter], [radius]]");
+    private static void validateLineParameters(float[] lineParameters) {
+        if(lineParameters.length != 3) {
+            throw new IllegalArgumentException("Invalid line parameters should be: [A, B, C]");
         }
     }
 
@@ -79,6 +91,44 @@ public abstract class CirclesIntersection {
         float ix2 = x5 + h * (y2 - y1) / distanceBetweenTwoCenters;
         float iy2 = y5 - h * (x2 - x1) / distanceBetweenTwoCenters;
         return new float[][]{{ix1, iy1}, {ix2, iy2}};
+    }
+
+    private static float[][] findCircleLineIntersects(float[][] circleParameters, float[] lineParameters) {
+        float coordinateOutOfScreen = 5000;
+        float[] point1;
+        float[] point2;
+        if (lineParameters[0] == 0) {
+            point1 = new float[]{0, (-lineParameters[0] * 0 - lineParameters[2]) / lineParameters[1]};
+            point2 = new float[]{coordinateOutOfScreen, (-lineParameters[0] * coordinateOutOfScreen - lineParameters[2]) / lineParameters[1]};
+        } else {
+            point1 = new float[]{-lineParameters[2] / lineParameters[0], 0};
+            point2 = new float[]{((-lineParameters[2] - lineParameters[1] * coordinateOutOfScreen) / lineParameters[0]), coordinateOutOfScreen};
+        }
+
+        float xCircleCenter = circleParameters[0][X_INDEX];
+        float yCircleCenter = circleParameters[0][Y_INDEX];
+        float radius = circleParameters[1][RADIUS_INDEX];
+        float x1 = point1[0], y1 = point1[1];
+        float x2 = point2[0], y2 = point2[1];
+
+        float dx = x2 - x1;
+        float dy = y2 - y1;
+        float a = dx * dx + dy * dy;
+        float b = 2 * dx * (x1 - xCircleCenter) + 2 * dy * (y1 - yCircleCenter);
+        double c = Math.pow((x1 - xCircleCenter), 2) + Math.pow((y1 - yCircleCenter), 2) - radius * radius;
+
+        double d = b * b - 4 * a * c;
+        if (d >= 0) {
+            double root = Math.sqrt(d);
+            double t1 = 2 * c / (-b + root);
+            double t2 = 2 * c / (-b - root);
+            double interX1 = x1 + dx * t1;
+            double interY1 = y1 + dy * t1;
+            double interX2 = x1 + dx * t2;
+            double interY2 = y1 + dy * t2;
+            return new float[][]{{(float) interX1, (float) interY1}, {(float) interX2, (float) interY2}};
+        }
+        return new float[0][0];
     }
 
     private static float distBetweenCircleCenters(float[] firstCircleCoord, float[] secondCircleCoord) {
